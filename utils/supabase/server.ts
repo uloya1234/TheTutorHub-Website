be09b97c5e0d23dffcf2d39becba3b522d46fe56
Umair Loya <mcpstutorhub@gmail.com>
2026-07-29 00:56:08 -0400

    Implement Supabase client creation with cookies

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -1,1 +1,28 @@
-
+1import { createServerClient } from "@supabase/ssr";
+2import { cookies } from "next/headers";
+3
+4const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+5const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
+6
+7export const createClient = (cookieStore: Awaited<ReturnType<typeof cookies>>) => {
+8  return createServerClient(
+9    supabaseUrl!,
+10    supabaseKey!,
+11    {
+12      cookies: {
+13        getAll() {
+14          return cookieStore.getAll()
+15        },
+16        setAll(cookiesToSet) {
+17          try {
+18            cookiesToSet.forEach(({ name, value, options }) => cookieStore.set(name, value, options))
+19          } catch {
+20            // The `setAll` method was called from a Server Component.
+21            // This can be ignored if you have middleware refreshing
+22            // user sessions.
+23          }
+24        },
+25      },
+26    },
+27  );
+28};
